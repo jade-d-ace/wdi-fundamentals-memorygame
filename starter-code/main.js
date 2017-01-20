6a7d8ff799ab83829b7dd76a59512fd4e390d31d
Jason Andrada <jasonandrada@Jasons-MacBook-Pro-2.local>
2017-01-20 09:58:58 -0500

    added if statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardOne!==cardThree) {
+	alert("Sorry, try again...");
+} else
+	alert("You found a match!");
+if (cardTwo!==cardThree) {
+	alert("Sorry, try again...")
+} 
+if (cardOne===cardTwo) {
+	alert("You found a match!!");
+} else 
+	alert("Sorry, try again...")
+if (cardThree===cardFour) {
+	alert("You found a match!!");
+} else 
+	alert("Sorry, try again...")
